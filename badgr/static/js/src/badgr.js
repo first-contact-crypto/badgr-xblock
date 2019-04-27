8b12ca1165231fa241ffac6b73068c0f0660f139
Peter Alexander <peter.alexander@prodatalab.com>
2019-04-27 11:13:30 -0600

    fixed getGrades

--- badgr/static/js/src/badgr.js
+++ badgr/static/js/src/badgr.js
@@ -5,7 +5,7 @@ function BadgrXBlock(runtime, element, data) {
     // var my_url = '/api/grades/v0/course_grade/' + data.course_id + '/users/?username=' + user
     var my_url = '/api/grades/v1/courses/' + data.course_id + '/?username=' + user
     var section_title = data.section_title;
-    var pass_mark = data.pass_mark;
+    var pass_mark = data.letter_grade;
     var award_message = data.award_message;
     var badge_slug = data.badge_slug;
     var motivation_message = data.motivation_message;
@@ -17,55 +17,55 @@ function BadgrXBlock(runtime, element, data) {
     function getGrades(data) {
         var section_scores = data['section_scores'];
         // Check that the section name specified in Xblock exists in Grades report
-        if (section_scores.hasOwnProperty(section_title)) {
-            var this_section = section_scores[String(section_title)];
-            var section_title_id = '#' + section_title
-            if ( parseFloat(this_section) >= pass_mark) {
-                $.ajax({
-                    type: "POST",
-                    url: handlerUrl,
-                    data:JSON.stringify({"name": "badgr"}),
-                    success: function(json) {
-                            // Just reload the page, the correct html with the badge will be displayed
-                            var onlyUrl = location.href.replace(location.search,'');
-                            window.location = onlyUrl;
-                            return false;
-                    },
-                    error : function(xhr,errmsg,err) {
-                        $('.badge-loader').hide();
-                        $('#lean_overlay').hide();
-                        $('#check-for-badge').remove();
-                        $('#results').html("<div>Oops! We have encountered an error, the badge " + 
-                        '"' +  badge_slug + '"' +  " does not exist. Please contact your support administrator."+
-                        "</div>"); // add the error to the dom
-                        console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
-                    }
-                });
-            }
-            else {
-                $.ajax({
+        // if (section_scores.hasOwnProperty(section_title)) {
+        //     var this_section = section_scores[String(section_title)];
+        //     var section_title_id = '#' + section_title
+        if (pass_mark == "Pass") {
+            $.ajax({
                 type: "POST",
-                url: noAwardUrl,
+                url: handlerUrl,
                 data:JSON.stringify({"name": "badgr"}),
                 success: function(json) {
-                    $('.badge-loader').hide();-
+                        // Just reload the page, the correct html with the badge will be displayed
+                        var onlyUrl = location.href.replace(location.search,'');
+                        window.location = onlyUrl;
+                        return false;
+                },
+                error : function(xhr,errmsg,err) {
+                    $('.badge-loader').hide();
                     $('#lean_overlay').hide();
-                    var $motivation = $('<p class="badgr-motivation">' 
-                    + motivation_message + '</p>' );
-                    $('.badgr_block').append($motivation);
                     $('#check-for-badge').remove();
-                    }
-                });
-            }
-        } else {
-            $('.badge-loader').hide();
-            $('#lean_overlay').hide();
-            alert(
-                'The module named ' + '"'+ section_title + '"' 
-                + ' does not exist in the Grades Report! Please check you have' 
-                + ' specified the correct module name for this badge.'
-            )
+                    $('#results').html("<div>Oops! We have encountered an error, the badge " + 
+                    '"' +  badge_slug + '"' +  " does not exist. Please contact your support administrator."+
+                    "</div>"); // add the error to the dom
+                    console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+                }
+            });
         }
+        else {
+            $.ajax({
+            type: "POST",
+            url: noAwardUrl,
+            data:JSON.stringify({"name": "badgr"}),
+            success: function(json) {
+                $('.badge-loader').hide();-
+                $('#lean_overlay').hide();
+                var $motivation = $('<p class="badgr-motivation">' 
+                + motivation_message + '</p>' );
+                $('.badgr_block').append($motivation);
+                $('#check-for-badge').remove();
+                }
+            });
+        }
+        // } else {
+        //     $('.badge-loader').hide();
+        //     $('#lean_overlay').hide();
+        //     alert(
+        //         'The module named ' + '"'+ section_title + '"' 
+        //         + ' does not exist in the Grades Report! Please check you have' 
+        //         + ' specified the correct module name for this badge.'
+        //     )
+        // }
     }
 
 
