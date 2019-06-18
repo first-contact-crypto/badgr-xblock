c00ee9ea804ae43bd15d87a9cab2b58347d669ab
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-06-18 10:52:13 -0600

    added refresh page message

--- badgr/static/js/src/badgr.js
+++ badgr/static/js/src/badgr.js
@@ -15,31 +15,31 @@ function BadgrXBlock(runtime, element, data) {
     var noAwardUrl = runtime.handlerUrl(element, "no_award_received");
     var onlyUrl = location.href.replace(location.search, "");
 
-    function scrollPage(target) {
-        if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $("html, body").animate(
-            {
-            scrollTop: target.offset().top
-            },
-            1000,
-            function() {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) {
-                // Checking if the target was focused
-                return false;
-            } else {
-                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-            }
-            }
-        );
-        }
-    }
+    // function scrollPage(target) {
+    //     if (target.length) {
+    //     // Only prevent default if animation is actually gonna happen
+    //     event.preventDefault();
+    //     $("html, body").animate(
+    //         {
+    //         scrollTop: target.offset().top
+    //         },
+    //         1000,
+    //         function() {
+    //         // Callback after animation
+    //         // Must change focus!
+    //         var $target = $(target);
+    //         $target.focus();
+    //         if ($target.is(":focus")) {
+    //             // Checking if the target was focused
+    //             return false;
+    //         } else {
+    //             $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+    //             $target.focus(); // Set focus again
+    //         }
+    //         }
+    //     );
+    //     }
+    // }
 
     function getGrades(data) {
         // var section_scores = data["section_scores"];
