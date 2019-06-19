f31ba69434bec5eb045b7e77059352145225cdc0
Peter Alexaxander <peter.alexander@prodatalab.com>
2019-06-19 17:27:17 -0600

    grades 10

--- badgr/static/js/src/badgr.js
+++ badgr/static/js/src/badgr.js
@@ -18,7 +18,7 @@ function BadgrXBlock(runtime, element, data) {
 
   function getGrades(data) {
     console.log("INFO In getGrades..")
-    console.log("INFO: In getGrades.. data is " + JSON.stringify(data.json())
+    alert("INFO: In getGrades.. data is " + JSON.stringify(data.json())
 
     if (data.success === "correct") {
       $.ajax({
@@ -29,7 +29,7 @@ function BadgrXBlock(runtime, element, data) {
           // Just reload the page, the correct html with the badge will be displayed
           var onlyUrl = location.href.replace(location.search, "");
           window.location = onlyUrl;
-          return false;
+          return true;
         },
         error: function(xhr, errmsg, err) {
           $(".badge-loader").hide();
@@ -44,6 +44,7 @@ function BadgrXBlock(runtime, element, data) {
               "</div>"
           ); // add the error to the dom
           console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+          return false
         }
       });
     } else {
@@ -65,69 +66,9 @@ function BadgrXBlock(runtime, element, data) {
 
 
 
-    var section_scores = data["section_scores"];
-    // Check that the section name specified in Xblock exists in Grades report
-    if (section_scores.hasOwnProperty(section_title)) {
-      var this_section = section_scores[String(section_title)];
-      var section_title_id = "#" + section_title;
-      if (parseFloat(this_section) >= pass_mark) {
-        $.ajax({
-          type: "POST",
-          url: handlerUrl,
-          data: JSON.stringify({ name: "badgr" }),
-          success: function(json) {
-            // Just reload the page, the correct html with the badge will be displayed
-            var onlyUrl = location.href.replace(location.search, "");
-            window.location = onlyUrl;
-            return false;
-          },
-          error: function(xhr, errmsg, err) {
-            $(".badge-loader").hide();
-            $("#lean_overlay").hide();
-            $("#check-for-badge").remove();
-            $("#results").html(
-              "<div>Oops! We have encountered an error, the badge " +
-                '"' +
-                badge_slug +
-                '"' +
-                " does not exist. Please contact your support administrator." +
-                "</div>"
-            ); // add the error to the dom
-            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
-          }
-        });
-      } else {
-        $.ajax({
-          type: "POST",
-          url: noAwardUrl,
-          data: JSON.stringify({ name: "badgr" }),
-          success: function(json) {
-            $(".badge-loader").hide();
-            $("#lean_overlay").hide();
-            var $motivation = $(
-              '<p class="badgr-motivation">' + motivation_message + "</p>"
-            );
-            $(".badgr_block").append($motivation);
-            $("#check-for-badge").remove();
-          }
-        });
-      }
-    } else {
-      $(".badge-loader").hide();
-      $("#lean_overlay").hide();
-      alert(
-        "The module named " +
-          '"' +
-          section_title +
-          '"' +
-          " does not exist in the Grades Report! Please check you have" +
-          " specified the correct module name for this badge."
-      );
-    }
-  }
 
   $("#check-for-badge").click(function(event) {
-    console.log("INFO in check-for-badge().click().. calling getGrades(data)")
+    alert("INFO in check-for-badge().click().. calling getGrades(data)")
 
     event.preventDefault();
     event.stopImmediatePropagation();
@@ -137,8 +78,49 @@ function BadgrXBlock(runtime, element, data) {
       type: "GET",
       url: my_url,
       success: function (data) {
-        console.log("INFO in check-for-badge().click().. calling getGrades(data)")
+        alert("INFO in check-for-badge().click().. calling getGrades(data)")
         getGrades(data)
     });
   });
 }
+
+
+
+// EDITED BY MY 
+// https://css-tricks.com/snippets/jquery/smooth-scrolling/
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+});
